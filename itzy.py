6098427b359052259e3804853578a3ae3e341580
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-06-29 19:21:19 +0800

    add GIF/video handler

--- itzy.py
+++ itzy.py
@@ -25,6 +25,7 @@ ITZY_ACCOUNTS = {
         'yunaspics',
         'yunasarchive',
         'picyuna',
+        'littIeyuna',
     ],
     'YEJI': [
         'hourlyyeji',
@@ -37,21 +38,6 @@ ITZY_ACCOUNTS = {
 }
 
 
-HELP_MESSAGE = """
-Hi! I am ITZYbot!
-
-List of commands:
-`!ping` : Check bot responsiveness.
-`!help` : Show this help message.
-`!ryujin`, `!yeji`, `!yuna`, `!lia`, `!chaeryeong` : Get a random pic of the member.
-`!all`: Get a random pic from accounts that post all members.
-`!random` : Get a random member.
-"""
-
-
-TARGET_CHANNEL = 726831180565184603
-
-
 api = twitter.Api(
     consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
     consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
@@ -60,7 +46,7 @@ api = twitter.Api(
 )
 
 
-client = commands.Bot(command_prefix='!')
+client = commands.Bot(command_prefix='!', description="Hi, I'm ITZYbot!")
 
 @client.event
 async def on_ready():
@@ -68,11 +54,11 @@ async def on_ready():
     print(f'Logged in as {client.user}')
     hourly_itzy.start()
 
-@client.command()
+@client.command(help='Check server response time')
 async def ping(ctx):
     await ctx.send(f'I am awake! ({round(client.latency*1000)}ms)')
 
-@client.command()
+@client.command(help='Get a random pic of the specified member')
 async def itz(ctx, member):
     if member is None or member.upper() not in ITZY_ACCOUNTS:
         await ctx.send("That's not a member of ITZY :cry:")
@@ -86,7 +72,7 @@ async def itz(ctx, member):
     for link in links:
         await ctx.send(link)
 
-@client.command()
+@client.command(help='Clear the specified amount of latest messages')
 async def clear(ctx, amount):
     if amount == None or amount < 1:
         await ctx.send('Please specify a positive number.')
@@ -95,16 +81,26 @@ async def clear(ctx, amount):
 
 @tasks.loop(seconds=60*60)
 async def hourly_itzy():
-    channel = client.get_channel(TARGET_CHANNEL)
+    channel = client.get_channel(int(os.environ['TARGET_CHANNEL']))
     print(f'Connected to channel {channel}')
 
     account_cat = random.choice(list(ITZY_ACCOUNTS.values()))
     tl = api.GetUserTimeline(screen_name=random.choice(account_cat))
+
     media_post = (random.choice(tl)).media
     while media_post == None or len(media_post) == 0:
         media_post = random.choice(tl).media
-    links = [media.media_url_https for media in media_post]
-    for link in links:
+
+    video_info = media_post[0].video_info
+    if video_info is not None:
+        if len(video_info['variants']) > 1:
+            link = video_info['variants'][1]['url']
+        else:
+            link = video_info['variants'][0]['url']
         await channel.send(link)
+    else:
+        links = [media.media_url_https for media in media_post]
+        for link in links:
+            await channel.send(link)
 
 client.run(os.environ['DISCORD_TOKEN'])
