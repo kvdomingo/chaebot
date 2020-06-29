75d76573348a9b35719e2017af8509ef6468047f
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-06-29 21:24:45 +0800

    add aliases to members add command for retrieving alias list

---
+++ README.md
@@ -0,0 +1,7 @@
+# ITZYbot
+A Discord bot to pull random ITZY pics from public fan accounts on Twitter and post them to a Discord server channel.
+
+## Usage
+The bot is currently in `alpha` phase. Shoot me an email to request access!
+
+If I have already sent you an access link (which should automatically add the bot to your selected server), you don't need to do anything else. Send a `!help` command to the bot (either by DM or server) to list all the commands.
--- itzy.py
+++ itzy.py
@@ -7,6 +7,43 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+ALIASES = {
+    'RANDOM': [
+        'sisi',
+        'bayan ko',
+        'bayan',
+        'mahusay',
+        'bayan',
+        'husay',
+        'huhusay',
+    ],
+    'RYUJIN': [
+        'ryujin',
+        'joanne',
+    ],
+    'LIA': [
+        'lia',
+        'julia',
+        'jisu',
+        'jisoo',
+        'choi',
+    ],
+    'YUNA': [
+        'yuna',
+        'hussey',
+    ],
+    'YEJI': [
+        'yeji',
+        'lucy',
+    ],
+    'CHAERYEONG': [
+        'chaeryeong',
+        'ryeong',
+        'judy',
+    ],
+}
+
+
 ITZY_ACCOUNTS = {
     'ALL': [
         'archiveitzy',
@@ -46,45 +83,8 @@ api = twitter.Api(
 )
 
 
-client = commands.Bot(command_prefix='!', description="Hi, I'm ITZYbot!")
-
-@client.event
-async def on_ready():
-    await client.change_presence(status=discord.Status.idle, activity=discord.Game(name='ITZY fancams'))
-    print(f'Logged in as {client.user}')
-    hourly_itzy.start()
-
-@client.command(help='Check server response time')
-async def ping(ctx):
-    await ctx.send(f'I am awake! ({round(client.latency*1000)}ms)')
-
-@client.command(help='Get a random pic of the specified member')
-async def itz(ctx, member):
-    if member is None or member.upper() not in ITZY_ACCOUNTS:
-        await ctx.send("That's not a member of ITZY :cry:")
-        return
+async def media_handler(ctx, member, **kwargs):
     account_cat = ITZY_ACCOUNTS[member.upper()]
-    tl = api.GetUserTimeline(screen_name=random.choice(account_cat))
-    media_post = (random.choice(tl)).media
-    while media_post == None or len(media_post) == 0:
-        media_post = random.choice(tl).media
-    links = [media.media_url_https for media in media_post]
-    for link in links:
-        await ctx.send(link)
-
-@client.command(help='Clear the specified amount of latest messages')
-async def clear(ctx, amount):
-    if amount == None or amount < 1:
-        await ctx.send('Please specify a positive number.')
-        return
-    await ctx.channel.purge(limit=amount)
-
-@tasks.loop(seconds=60*60)
-async def hourly_itzy():
-    channel = client.get_channel(int(os.environ['TARGET_CHANNEL']))
-    print(f'Connected to channel {channel}')
-
-    account_cat = random.choice(list(ITZY_ACCOUNTS.values()))
     tl = api.GetUserTimeline(screen_name=random.choice(account_cat))
 
     media_post = (random.choice(tl)).media
@@ -97,10 +97,77 @@ async def hourly_itzy():
             link = video_info['variants'][1]['url']
         else:
             link = video_info['variants'][0]['url']
-        await channel.send(link)
+        await ctx.send(link)
     else:
         links = [media.media_url_https for media in media_post]
         for link in links:
-            await channel.send(link)
+            await ctx.send(link)
+
+
+client = commands.Bot(command_prefix='!', description="Hi, I'm ITZYbot!")
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game(name='ITZY fancams'))
+    print(f'Logged in as {client.user}')
+    hourly_itzy.start()
+
+
+@client.command(aliases=['wake'], help='Check server response time')
+async def ping(ctx):
+    await ctx.send(f'I am awake! ({round(client.latency*1000)} ms)')
+
+
+@client.command(help='Get a random pic of the specified member')
+async def itz(ctx, person):
+    if person is None:
+        member = random.choice(ITZY_ACCOUNTS.keys())
+        await media_handler(ctx, member)
+
+    if person.upper() in ITZY_ACCOUNTS.keys():
+        member = person.upper()
+        await media_handler(ctx, member)
+    else:
+        member = None
+        for key in ALIASES.keys():
+            if person in ALIASES[key]:
+                member = key
+                break
+        if member is None:
+            ctx.send("Sorry, I don't recognize that member or alias :cry:")
+            return
+        await media_handler(ctx, member)
+
+@client.command(aliases=['get-aliases'], help='Get a list of member aliases')
+async def get_aliases(ctx, *args):
+    if len(args) == 0:
+        message = '```\n'
+        for key, val in ALIASES.items():
+            message += f'{key}: {", ".join(val)}\n'
+        message += '\n```'
+        await ctx.send(message)
+    elif len(args) == 1:
+        args[0] = args[0].upper()
+        message = f'```\n{args[0]}: {ALIASES[args[0]]}\n```'
+        await ctx.send(message)
+
+
+@client.command(help='Clear the specified amount of latest messages')
+async def clear(ctx, amount):
+    if amount == None or int(amount) < 1:
+        await ctx.send('Please specify a positive number.')
+        return
+    await ctx.channel.purge(limit=int(amount))
+
+
+@tasks.loop(seconds=60*60)
+async def hourly_itzy():
+    channel = client.get_channel(int(os.environ['TARGET_CHANNEL']))
+    print(f'Connected to channel {channel}')
+
+    member = random.choice(list(ITZY_ACCOUNTS.keys()))
+    media_handler(channel, member)
+
 
 client.run(os.environ['DISCORD_TOKEN'])
