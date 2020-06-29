f6cbc51a4f7737f54547c381d384c3987fa20d01
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-06-29 17:04:16 +0800

    migrated main logic to functional async added hourly post scheduler

--- itzy.py
+++ itzy.py
@@ -1,19 +1,15 @@
 import os
-import discord
-import twitter
 import random
+import twitter
+import discord
+from discord.ext import commands, tasks
+from itertools import cycle
 from dotenv import load_dotenv
 
 load_dotenv()
 
-api = twitter.Api(
-    consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
-    consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
-    access_token_key=os.environ['TWITTER_ACCESS_KEY'],
-    access_token_secret=os.environ['TWITTER_ACCESS_SECRET'],
-)
 
-ACCOUNTS = {
+ITZY_ACCOUNTS = {
     'ALL': [
         'archiveitzy',
         'ITZYofficial',
@@ -42,48 +38,71 @@ ACCOUNTS = {
     ],
 }
 
+
 HELP_MESSAGE = """
 Hi! I am ITZYbot!
 
 List of commands:
-`!wake` : Check if the bot is online.
+`!ping` : Check bot responsiveness.
 `!help` : Show this help message.
 `!ryujin`, `!yeji`, `!yuna`, `!lia`, `!chaeryeong` : Get a random pic of the member.
+`!all`: Get a random pic from accounts that post all members.
 `!random` : Get a random member.
 """
 
 
-class ItzyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user}')
-
-    async def on_message(self, message):
-        if message.author == client.user:
-            return
-
-        if message.content.startswith('!'):
-            query = message.content[1:]
-
-            if query == 'wake':
-                await message.channel.send('I am awake!')
-
-            if query == 'help':
-                await message.channel.send(HELP_MESSAGE)
-
-            if query.upper() in ACCOUNTS.keys() or query.upper() == 'RANDOM':
-                if query.upper() == 'RANDOM':
-                    account_keys = list(ACCOUNTS.keys())
-                    account_cat = ACCOUNTS[account_keys[random.randint(0, len(account_keys)-1)]]
-                else:
-                    account_cat = ACCOUNTS[query.upper()]
-                tl = api.GetUserTimeline(screen_name=account_cat[random.randint(0, len(account_cat)-1)])
-                media_post = tl[random.randint(0, len(tl)-1)].media
-                while media_post == None or len(media_post) == 0:
-                    media_post = tl[random.randint(0, len(tl)-1)].media
-                links = [media.media_url_https for media in media_post]
-                for link in links:
-                    await message.channel.send(link)
+TARGET_CHANNEL = 726831180565184603
 
 
-client = ItzyClient()
+api = twitter.Api(
+    consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+    consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+    access_token_key=os.environ['TWITTER_ACCESS_KEY'],
+    access_token_secret=os.environ['TWITTER_ACCESS_SECRET'],
+)
+
+
+client = commands.Bot(command_prefix='!')
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game(name='ITZY fancams'))
+    print(f'Logged in as {client.user}')
+    hourly_itzy.start()
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'I am awake! ({round(client.latency*1000)}ms)')
+
+@client.command()
+async def itz(ctx, member):
+    account_cat = ITZY_ACCOUNTS[member.upper()]
+    tl = api.GetUserTimeline(screen_name=random.choice(account_cat))
+    media_post = (random.choice(tl)).media
+    while media_post == None or len(media_post) == 0:
+        media_post = random.choice(tl).media
+    links = [media.media_url_https for media in media_post]
+    for link in links:
+        await ctx.send(link)
+
+@client.command()
+async def clear(ctx, amount):
+    if amount == None or amount < 1:
+        await ctx.send('Please specify a positive number.')
+    await ctx.channel.purge(limit=amount)
+
+@tasks.loop(seconds=60*60)
+async def hourly_itzy():
+    channel = client.get_channel(TARGET_CHANNEL)
+    print(f'Connected to channel {channel}')
+
+    account_cat = random.choice(list(ITZY_ACCOUNTS.values()))
+    tl = api.GetUserTimeline(screen_name=random.choice(account_cat))
+    media_post = (random.choice(tl)).media
+    while media_post == None or len(media_post) == 0:
+        media_post = random.choice(tl).media
+    links = [media.media_url_https for media in media_post]
+    for link in links:
+        await channel.send(link)
+
 client.run(os.environ['DISCORD_TOKEN'])
