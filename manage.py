db07849c249077fb5dc488bf9f0ba2c94d6a9772
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-07-11 19:32:24 +0800

    add alembic for handling migrations

--- manage.py
+++ manage.py
@@ -5,15 +5,21 @@ from argparse import ArgumentParser
 def main():
     parser = ArgumentParser()
     parser.add_argument('cmd', type=str)
+    parser.add_argument('-m', '--message', type=str)
     args = parser.parse_args()
 
     if len(sys.argv) < 2:
         parser.print_help(sys.stderr)
         sys.exit(0)
 
+    if args.cmd == 'makemigrations':
+        from manage import makemigrations
+        makemigrations.main(args.message)
+
     if args.cmd == 'migrate':
         from manage import migrate
         migrate.main()
+
 
 
 if __name__ == '__main__':
