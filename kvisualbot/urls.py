c593f8fe5fa620a5b3b3dc29f5a2e5c4e941c9b4
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2021-01-09 19:49:10 +0800

    add documentation redirect

--- kvisualbot/urls.py
+++ kvisualbot/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from bot.views import documentation
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1.0/', include('bot.urls')),
+    path('', documentation),
 ]
