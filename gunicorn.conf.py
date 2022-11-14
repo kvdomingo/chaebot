d405d99f83a1871ad0b1c830529cbc6df599a4b1
Kenneth V. Domingo <hello@kvdomingo.xyz>
2022-11-14 21:36:09 +0800

    update workers

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,16 @@
 wsgi_app = "kvisualbot.wsgi"
 
-worker_class = "gthread"
+worker_class = "gevent"
 workers = 1
-threads = 2
+
 timeout = 30
+graceful_timeout = 10
+keepalive = 65
 
 errorlog = "-"
 accesslog = "-"
 loglevel = "debug"
 capture_output = True
+
+forwarded_allow_ips = "*"
+proxy_allow_ips = "*"
