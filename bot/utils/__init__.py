3c6c94a1cb8392394ff3f964dfcfb83cf5ad63f0
Kenneth Domingo <hello@kvdomingo.xyz>
2023-03-12 16:22:22 +0800

    feat: update schedule embed format (#32)

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,4 +1,5 @@
 from bot.utils.generate_embed import SeverityLevel, generate_embed
+from bot.utils.generate_schedule_fields import generate_schedule_fields
 
 
 def escape_quote(queries: list[str] | tuple[str]) -> list[str]:
@@ -8,5 +9,6 @@ def escape_quote(queries: list[str] | tuple[str]) -> list[str]:
 __all__ = [
     "escape_quote",
     "generate_embed",
+    "generate_schedule_fields",
     "SeverityLevel",
 ]
