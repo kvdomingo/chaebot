e83759303861513b052cbeaadae0ddaf2946f0c2
Kenneth V. Domingo <hello@kvd.studio>
2024-08-04 14:46:12 +0800

    chore: update packages

--- bot/firestore.py
+++ bot/firestore.py
@@ -1,16 +1,6 @@
-import json
-import os
+from firebase_admin import firestore_async, initialize_app
 
-from django.conf import settings
-from firebase_admin import credentials, firestore_async, initialize_app
-
-if (adc := os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")) is None:
-    creds = credentials.Certificate(str(settings.BASE_DIR / "gcp_sa_key.json"))
-else:
-    creds = credentials.Certificate(json.loads(adc))
-
-
-initialize_app(creds)
+initialize_app()
 
 
 def get_firestore_client():
--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,9 +1,13 @@
+import * as path from "node:path";
 import react from "@vitejs/plugin-react";
-import * as path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
