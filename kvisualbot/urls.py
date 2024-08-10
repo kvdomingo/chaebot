c121ace04c9f7f8b5040e23801dcccd9d9d27dc8
Kenneth V. Domingo <hello@kvd.studio>
2024-08-10 17:31:08 +0800

    ci: add gitea workflow

--- kvisualbot/urls.py
+++ kvisualbot/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http.response import HttpResponse
 from django.urls import include, path
 
 from kvisualbot.views import index
@@ -6,5 +7,6 @@ from kvisualbot.views import index
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1.0/", include("bot.urls")),
+    path("api/health/", lambda r: HttpResponse(b"ok")),
     path("", index),
 ]
